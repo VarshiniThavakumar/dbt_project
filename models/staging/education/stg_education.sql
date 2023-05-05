7198a266b247b1c6d93ae50e0310e3173a90e9d4
varshinithavakumar@kubrickgroup.com <varshinithavakumar@kubrickgroup.com>
2023-05-05 12:42:23 +0000

    updates

--- models/staging/education/stg_education.sql
+++ models/staging/education/stg_education.sql
@@ -1,6 +1,5 @@
 select 
-    id 
-    ,gender 
+    gender 
     , ethnic_grp 
     ,parent_edu 
     ,lunch_type 
@@ -14,4 +13,18 @@ select
     ,math_score 
     ,reading_score 
     ,writing_score 
-from {{ source('education', 'edu_data') }}
+from {{ source('education', 'edu_data') }} as edu
+WHERE edu.gender IS NOT NULL 
+    and edu.ethnic_grp IS NOT NULL
+    and edu.parent_edu IS NOT NULL
+    and edu.lunch_type IS NOT NULL
+    and edu.test_prep IS NOT NULL 
+    and edu.parent_marital_status IS NOT NULL
+    and edu.sport_practise IS NOT NULL
+    and edu.isFirstChild IS NOT NULL
+    and edu.num_siblings IS NOT NULL
+    and edu.transport IS NOT NULL
+    and edu.weekly_study IS NOT NULL
+    and edu.math_score IS NOT NULL
+    and edu.reading_score IS NOT NULL
+    and edu.writing_score IS NOT NULL
